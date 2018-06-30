3ad36785617136cfa785f6b18e53ada93bc916c1
Yucheng Chuang <ducksteven@gmail.com>
2018-06-30 17:43:55 +0900

    test lah

--- server/test/src/lib/Parser.test.ts
+++ server/test/src/lib/Parser.test.ts
@@ -1,23 +1,33 @@
 import { Parser } from '../../../src/lib/Parser';
 import { DiagnosticSeverity, Diagnostic } from "vscode-languageserver/lib/main";
 
-describe('validateTextDocument()', () => {
-  test('returns no diagnostic when no error', () => {
-    const src = "class Annoymous"
-    const parser = new Parser()
-    expect(parser.validateSource(src)).toHaveLength(0)
+describe('Parser', () => {
+  describe('validateSource()', () => {
+    test('returns no diagnostic when no error', () => {
+      const src = "class Annoymous"
+      const parser = new Parser()
+      expect(parser.validateSource(src)).toHaveLength(0)
+    })
+
+    test('returns diagnostics when there are errors', () => {
+      const src = "a ="
+      const expected: Diagnostic = {
+        severity: DiagnosticSeverity.Error,
+        range: { start: { line: 0, character: 3 }, end: { line: 0, character: 3 }},
+        message: "1:4 unexpected end of input",
+        source: 'coffee'
+      }
+
+      const parser = new Parser()
+      expect(parser.validateSource(src)).toEqual([expected])
+    })
   })
 
-  test('returns diagnostics when there are errors', () => {
-    const src = "a ="
-    const expected: Diagnostic = {
-      severity: DiagnosticSeverity.Error,
-      range: { start: { line: 0, character: 3 }, end: { line: 0, character: 3 }},
-      message: "1:4 unexpected end of input",
-      source: 'coffee'
-    }
+  describe('getSymbolsFromSource()', () => {
+    test.skip('returns all symbols including those in closure', () => {
+    })
 
-    const parser = new Parser()
-    expect(parser.validateSource(src)).toEqual([expected])
-  })
-})
+    test.skip('returns only global symbols if asked', () => {
+    })
+  });
+});
